ac6a46ee3ca79acddb5197b47400410cdf84425b
Tarek Saade <Tarek Saade>
2017-01-14 16:59:51 -0500

    angular assignment 6: Completed

--- dist/scripts/services/fixtures.js
+++ dist/scripts/services/fixtures.js
@@ -52,7 +52,11 @@
 
         Fixtures.getAlbum = function() {
             return albumPicasso;
-        }; 
+        };
+        
+        Fixtures.getCollection = function(numberOfAlbums) {
+            return [albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso, albumPicasso];
+        };
         
         return Fixtures;
         
